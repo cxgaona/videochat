c6335a66105c8f2820a4851cf3d751ee0cdb152e
Christian Gaona  .:: DINARDAP <christian.gaona@dinardap.int>
2019-07-23 12:08:18 -0500

    videollamada

--- dig/index.php
+++ dig/index.php
@@ -11,12 +11,24 @@ include "db.php";
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
     <script type="text/javascript">
-        function ajax() {
-            var cookieUsuario = Cookies.get('nombreUsuario');            
+        function validacionCookie() {
+            /*var cookieVideollamada = Cookies.get('videollamada');
+            if (cookieVideollamada != undefined) {
+                videollamadaSaliente()
+            }*/
+        }
+
+        function video(img) {
+            document.querySelector('#miImg').src = img;
+            document.getElementById('texto').value = img;
+        }
+
+        function cargaChat() {
+            var cookieUsuario = Cookies.get('nombreUsuario');
             if (cookieUsuario != undefined) {
                 $('#nombreCookie').text('USUARIO: ' + cookieUsuario);
                 $('#nombre').css('display', 'none')
-            } 
+            }
             var req = new XMLHttpRequest();
             req.onreadystatechange = function() {
                 if (req.readyState == 4 && req.status == 200) {
@@ -27,7 +39,7 @@ include "db.php";
             req.send();
         }
         setInterval(function() {
-            ajax();
+            cargaChat();
         }, 1000);
     </script>
     <style>
@@ -38,58 +50,174 @@ include "db.php";
     </style>
 </head>
 
-<body onload="ajax()">
-    <div id="contenedor">
-        <div id="caja-chat" class="divScroll">
-            <div id="chat"></div>
-        </div>
-        <form method="POST" action="index.php" enctype="multipart/form-data">
-            <input id="nombre" type="text" name="nombre" placeholder="Ingresa tu nombre">
-            <span id="nombreCookie"></span>
-            <textarea name="mensaje" placeholder="Ingresa el mensaje"></textarea>
-            <input type="file" name="archivo" />
-            <input type="submit" name="enviar" value="Enviar">
-            <input type="submit" name="borrarChat" value="Limpiar Chat">
-        </form>
+<body onload="validacionCookie(),cargaChat(), videollamadaSaliente()">
+    <table>
+        <tr>
+            <td style="vertical-align: top; text-align: center">
+                <div>
+                    <video autoplay id="video" style="width: 300px"></video>
+                    <canvas id='miCanvas'></canvas>
+                    <img id='miImg'>
 
-        <?php
-        if (isset($_POST['enviar'])) {
-            $nombre = "";
-            if (isset($_COOKIE["nombreUsuario"])) {
-                $nombre = $_COOKIE['nombreUsuario'];
-            } else {
-                $nombre = $_POST['nombre'];
-            }
-            $mensaje = $_POST['mensaje'];
-            $archivo = "";
-            $consulta = "";
-            if (!empty($_FILES['archivo']['name'])) {
-                copy($_FILES['archivo']['tmp_name'], 'archivosEnviados/' . date('Ymdhis') . $_FILES['archivo']['name']);
-                $archivo = 'archivosEnviados/' . date('Ymdhis') . $_FILES['archivo']['name'];
-            }
+                    <script>
+                        
+                        function videollamadaSaliente() {
+                            var req = new XMLHttpRequest();
+                            req.onreadystatechange = function() {
+                                console.log(req.status);
+                                if (req.readyState == 4 && req.status == 200) {
+                                    
+                                }
+                            }
+                            req.open('GET', 'server.php', true);
+                            req.send();
+                            var bandera = 0;
+                            //si el navegador soporta esta caracteristica 
 
-            if (empty($_POST['mensaje'])&&empty($_FILES['archivo']['name'])) {
-                echo "<script>alert('No puedes enviar mensajes vacíos')</script>";
-                
-            }else{
-                $consulta = "INSERT INTO chat (nombre, mensaje,url_archivo) VALUES ('$nombre' ,'$mensaje','$archivo')";
-            $ejecutar = $conexion->query($consulta);
-            if ($ejecutar) {
-                echo "<embed loop = 'false' src='beep.mp3' hidden='true' autoplay='true'> ";
-            }
-            echo "<script>Cookies.set('nombreUsuario','" . $nombre . "');</script>";
+                            window.URL = window.URL || window.webkitURL;
+                            navigator.getUserMedia = (navigator.getUserMedia ||
+                                navigator.webkitGetUserMedia ||
+                                navigator.mozGetUserMedia ||
+                                navigator.msGetUserMedia);
 
-            }
+                            if (navigator.getUserMedia) //si es compatible
+                            {
+                                navigator.getUserMedia({
+                                        audio: false,
+                                        video: true
+                                    },
+                                    function(vid) {
+                                        bandera = 1;
+                                        //document.querySelector('video').src = window.URL.createObjectURL(vid);
+                                        document.querySelector('video').srcObject = vid;
+                                    },
+                                    function(err) {
+                                        console.log("The following error occured: " + err);
+                                    }
+                                );
+                            } else {
+                                console.log("getUserMedia not supported");
+                            }
 
-            
-            
-            
-        }
-        if (isset($_POST['borrarChat'])) {
-            $consulta = "TRUNCATE TABLE CHAT";
-            $ejecutar = $conexion->query($consulta);
-        }
-        ?>
-    </div>
+                            window.requestAnimFrame = (function(callback) {
+                                return window.requestAnimationFrame ||
+                                    window.webkitRequestAnimationFrame ||
+                                    window.mozRequestAnimationFrame ||
+                                    window.oRequestAnimationFrame ||
+                                    window.msRequestAnimationFrame ||
+                                    function(callback) {
+                                        window.setTimeout(callback, 1000 / 100);
+                                    };
+                            })();
+
+                            function dFrame(ctx, video, canvas) {
+                                ctx.drawImage(video, 0, 0, 200, 150);
+                                var dataURL = canvas.toDataURL('image/jpeg', 0.2);
+                                if (bandera != 0) {
+                                    send(dataURL); //este es el socket  
+                                }
+
+                                requestAnimFrame(function() {
+                                    setTimeout(function() {
+                                        dFrame(ctx, video, canvas);
+                                    }, 200)
+                                });
+                            }
+                            window.addEventListener('load', init); //inicia cuando carga la pagina
+                            function init() {
+                                var canvas = document.querySelector('#miCanvas');
+                                var video = document.querySelector('video');
+                                var ctx = canvas.getContext('2d');
+                                dFrame(ctx, video, canvas);
+                            }
+                            function video(img)
+                                {
+                                    document.querySelector('#miImg').src = img;                                    
+                                }
+
+                        }
+                    </script>
+
+
+
+                </div>
+            </td>
+            <td>
+                <div id="contenedor">
+                    <div id="caja-chat" class="divScroll">
+                        <div id="chat"></div>
+                    </div>
+                    <form method="POST" action="index.php" enctype="multipart/form-data">
+                        <input id="nombre" type="text" name="nombre" placeholder="Ingresa tu nombre">
+                        <span id="nombreCookie"></span>
+                        <textarea name="mensaje" placeholder="Ingresa el mensaje"></textarea>
+                        <input type="file" name="archivo" />
+                        <input type="submit" name="enviar" value="Enviar">
+                        <input type="submit" name="borrarChat" value="Limpiar Chat">
+                        <input type="button" id="videollamada" onclick="videollamadaSaliente()" value="Videollamada">
+                        <input type="button" id="no se" onclick="abrirCliente()" value="Tu madre">
+                    </form>
+
+                    <?php
+                    if (isset($_POST['enviar'])) {
+
+                        $nombre = "";
+                        if (isset($_COOKIE["nombreUsuario"])) {
+                            $nombre = $_COOKIE['nombreUsuario'];
+                        } else {
+                            $nombre = $_POST['nombre'];
+                        }
+                        $mensaje = $_POST['mensaje'];
+                        $archivo = "";
+                        $consulta = "";
+                        if (!empty($_FILES['archivo']['name'])) {
+                            copy($_FILES['archivo']['tmp_name'], 'archivosEnviados/' . date('Ymdhis') . $_FILES['archivo']['name']);
+                            $archivo = 'archivosEnviados/' . date('Ymdhis') . $_FILES['archivo']['name'];
+                        }
+
+                        if (empty($_POST['mensaje']) && empty($_FILES['archivo']['name'])) {
+                            echo "<script>alert('No puedes enviar mensajes vacíos')</script>";
+                        } else {
+                            $consulta = "INSERT INTO chat (nombre, mensaje,url_archivo) VALUES ('$nombre' ,'$mensaje','$archivo')";
+                            $ejecutar = $conexion->query($consulta);
+                            if ($ejecutar) {
+                                echo "<embed loop = 'false' src='beep.mp3' hidden='true' autoplay='true'> ";
+                            }
+                            echo "<script>Cookies.set('nombreUsuario','" . $nombre . "');</script>";
+                        }
+                    }
+                    if (isset($_POST['borrarChat'])) {
+                        $consulta = "TRUNCATE TABLE CHAT";
+                        $ejecutar = $conexion->query($consulta);
+                    }
+                    ?>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+
 </body>
+<script>
+
+    function abrirCliente() {
+        Server = new FancyWebSocket('ws://10.0.0.168:12345');
+        console.log('inicio funcion');
+        Server.bind('open', function()
+        {
+        });
+        Server.bind('close', function( data ) 
+        {
+        });
+        Server.bind('message', function( payload ) 
+        {
+        });
+        Server.connect();        
+        console.log('despues del conect');
+    }
+    
+</script>
+<script language="javascript" src="js/jquery-1.7.2.min.js"></script>
+<script language="javascript" src="js/fancywebsocket.js"></script>
+
 </html>
---
+++ dig/server.php
@@ -0,0 +1,48 @@
+<?php
+set_time_limit(0);
+require 'clases/class.PHPWebSocket.php';
+function wsOnMessage($clientID, $message, $messageLength, $binary) 
+{
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
+	if ($messageLength == 0) {
+		$Server->wsClose($clientID);
+		return;
+	}
+	if ( sizeof($Server->wsClients) == 1 )
+		$Server->wsSend($clientID, "");
+	else
+		foreach ( $Server->wsClients as $id => $client )
+			//if ( $id != $clientID )
+				//$Server->wsSend($id, "Visitor $clientID ($ip) said \"$message\"");
+				//aqui recibimos la accion con los demas parametros e identificadores
+				$Server->wsSend($id,$message);
+}
+function wsOnOpen($clientID)
+{
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
+	$Server->log( "" );
+	foreach ( $Server->wsClients as $id => $client )
+		if ( $id != $clientID )
+			$Server->wsSend($id, "");
+}
+function wsOnClose($clientID, $status) {
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
+	$Server->log( "" );
+	foreach ( $Server->wsClients as $id => $client )
+		$Server->wsSend($id, "");
+}
+$Server = new PHPWebSocket();
+$Server->bind('message', 'wsOnMessage');
+$Server->bind('open', 'wsOnOpen');
+$Server->bind('close', 'wsOnClose');
+if($_SERVER['REMOTE_ADDR']=="::1"){
+ 	$ipServidor="127.0.0.1";
+}else{
+	$ipServidor=$_SERVER['REMOTE_ADDR'];
+}
+$Server->wsStartServer($ipServidor,12345);
+//$Server->wsStartServer('192.168.43.206',12345);
+?>
